14b1240291bb56e3921c1e8c73bd4fc8c0b1b4e0
IsThaTaSuPra <glepape@myges.fr>
2025-07-03 09:53:03 +0200

    push of all did yesterday

---
+++ app/src/main/java/com/example/budgetbuddytravel/utils/CategorieActivity.java
@@ -0,0 +1,13 @@
+package com.example.budgetbuddytravel.utils;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.budgetbuddytravel.R;
+
+public class CategorieActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_categorie);
+    }
+}
