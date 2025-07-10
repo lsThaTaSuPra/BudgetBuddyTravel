f9068eef71ddf3b6cc7ca2c4cea668f7ecc9db0b
Alexandre TANGUY <alexandretang17@gmail.com>
2025-07-10 17:12:07 +0200

    feat: ajout de l'héritage

---
+++ app/src/main/java/com/example/budgetbuddytravel/model/Personne.java
@@ -0,0 +1,39 @@
+package com.example.budgetbuddytravel.model;
+
+public class Personne {
+    protected int id;
+    protected String nom;
+    protected String email;
+
+    public Personne() {}
+
+    public Personne(int id, String nom, String email) {
+        this.id = id;
+        this.nom = nom;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
